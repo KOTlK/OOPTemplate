ad4cdefdf23a11e440bf0afc7973bb6940039670
Konstantin <rojuty@gmail.com>
2024-04-19 16:51:01 +0700

    sample removed, sample scene added

--- Assets/~Sample/CharacterInput.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class CharacterInput : MonoBehaviour{
-    public Vector3 MoveDirection;
-    public float   LookDirection;
-    public bool    Shooting;
-    
-    public abstract void Execute();
-}
