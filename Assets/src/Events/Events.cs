331c998d61cce865c7f9f72b37be7dd9e2db930d
Konstantin <rojuty@gmail.com>
2024-05-16 13:57:43 +0700

    added pool, tasks revert into oop, added basic tasks primitives such as sequences, parallel tasks, covered it with tests

--- Assets/src/Events/Events.cs
+++ Assets/src/Events/Events.cs
@@ -5,7 +5,7 @@ using Unity.Collections;
 using static Unity.Collections.LowLevel.Unsafe.UnsafeUtility;
 using static UnityEngine.Assertions.Assert;
 
-public unsafe class EventData{
+public unsafe class EventData {
     public void*     Data;
     public int       Count;
     public int       Capacity;
@@ -14,7 +14,7 @@ public unsafe class EventData{
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Create<T>(int initialCapacity, Allocator allocator)
-    where T : unmanaged{
+    where T : unmanaged {
         Count      = 0;
         Capacity   = initialCapacity;
         Allocator  = allocator;
@@ -23,32 +23,32 @@ public unsafe class EventData{
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Free(){
+    public void Free() {
         FreeTracked(Data, Allocator);
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public T Get<T>(int index)
-    where T : unmanaged{
+    where T : unmanaged {
         return ReadArrayElement<T>(Data, index);
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ref T GetByRef<T>(int index)
-    where T : unmanaged{
+    where T : unmanaged {
         return ref ArrayElementAsRef<T>(Data, index);
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public void Clear(){
+    public void Clear() {
         Count = 0;
     }
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void Push<T>(T evnt)
-    where T : unmanaged{
+    where T : unmanaged {
         IsTrue(typeof(T) == BindedType);
-        if(Count >= Capacity){
+        if(Count >= Capacity) {
             Capacity = Capacity << 1;
             var newData = MallocTracked(sizeof(T) * Capacity, AlignOf<T>(), Allocator, 0);
             MemMove(newData, Data, sizeof(T) * Count);
@@ -60,25 +60,24 @@ public unsafe class EventData{
     }
 }
 
-public class Events : IDisposable{
+public class Events : IDisposable {
     public delegate void EventHandler(EventData events);
-    public Dictionary<Type, EventData>           AllEvents = new();
-    public Dictionary<Type, EventHandler>        Handlers = new();
+    public Dictionary<Type, EventData>    AllEvents = new();
+    public Dictionary<Type, EventHandler> Handlers = new();
     
     private readonly int _initialCapacity = 30;
     
-    public Events(){
-    }
+    public Events() {}
     
-    public Events(int initialCapacity){
+    public Events(int initialCapacity) {
         _initialCapacity = initialCapacity;
     }
     
     public void RaiseEvent<T>(T evnt)
-    where T : unmanaged{
+    where T : unmanaged {
         var type = typeof(T);
         
-        if(!AllEvents.ContainsKey(type)){
+        if(!AllEvents.ContainsKey(type)) {
             var eventData = new EventData();
             eventData.Create<T>(_initialCapacity, Allocator.Persistent);
             AllEvents.Add(type, eventData);
@@ -89,10 +88,10 @@ public class Events : IDisposable{
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public void HandleEvents<T>()
-    where T : unmanaged{
+    where T : unmanaged {
         var type = typeof(T);
         
-        if(AllEvents.ContainsKey(type)){
+        if(AllEvents.ContainsKey(type)) {
             IsTrue(Handlers.ContainsKey(type));
             Handlers[type].Invoke(AllEvents[type]);
             AllEvents[type].Clear();
@@ -100,9 +99,9 @@ public class Events : IDisposable{
     }
     
     public void AddHandler<T>(EventHandler handler)
-    where T : unmanaged{
+    where T : unmanaged {
         var type = typeof(T);
-        if(!Handlers.ContainsKey(type)){
+        if(!Handlers.ContainsKey(type)) {
             Handlers.Add(type, delegate{});
         }
         
@@ -110,7 +109,7 @@ public class Events : IDisposable{
     }
     
     public void RemoveHandler<T>(EventHandler handler)
-    where T : unmanaged{
+    where T : unmanaged {
         var type = typeof(T);
         
         IsTrue(Handlers.ContainsKey(type));
@@ -118,9 +117,11 @@ public class Events : IDisposable{
         Handlers[type] -= handler;
     }
     
-    public void Dispose(){
-        foreach(var (type, events) in AllEvents){
+    public void Dispose() {
+        foreach(var (type, events) in AllEvents) {
             events.Free();
         }
+        AllEvents.Clear();
+        Handlers.Clear();
     }
 }
